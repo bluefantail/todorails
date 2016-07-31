9cdfe170c1a930ca14c56dd54046c5cb3d836058
Matt Fannin <mattfannin@acidic.co.nz>
2016-07-31 22:16:01 +1200

    Controller tidyups.

--- app/controllers/desk_controller.rb
+++ app/controllers/desk_controller.rb
@@ -1,15 +1,5 @@
 class DeskController < ApplicationController
-  def create
-  	# @note = Note.create! job_params
-  	# render json: new_note and return
-  	render plain: params[:note].inspect
-  end
   def index
   	@notes = Note.all
   end
-
-  private
-   	def job_params
-  		params.require(:title).permit(:description, :due)
-  	end
 end
